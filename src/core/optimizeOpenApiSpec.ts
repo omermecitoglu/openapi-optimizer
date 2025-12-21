f00d75ec78ccc1cd0f0042bf69365d4c32192073
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-12-21 14:32:42 +0700

    feat: optimizeOpenApiSpec (exported)

--- src/core/optimizeOpenApiSpec.ts
+++ src/core/optimizeOpenApiSpec.ts
@@ -1,3 +1,20 @@
-export function optimizeOpenApiSpec() {
-  // do the magic
+import { handlePathItem } from "./handlePathItem";
+import type { OpenApiDocument } from "@omer-x/openapi-types";
+
+export function optimizeOpenApiSpec(inputSpec: OpenApiDocument): OpenApiDocument {
+  const outputSpec = structuredClone(inputSpec);
+  const storedSchemas = outputSpec.components?.schemas ?? {};
+  if (outputSpec.paths) {
+    const entries = Object.entries(outputSpec.paths).map(([pathName, pathItem]) => {
+      return [pathName, handlePathItem(pathItem, storedSchemas)] as const;
+    });
+    outputSpec.paths = Object.fromEntries(entries);
+  }
+  if (Object.keys(storedSchemas).length) {
+    outputSpec.components = {
+      ...outputSpec.components,
+      schemas: storedSchemas,
+    };
+  }
+  return outputSpec;
 }
